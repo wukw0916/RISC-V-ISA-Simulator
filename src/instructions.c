d8bcc5553ce29a8a356a446a2507cf89b31a6393
wukw <wukw@ljmicro.cn>
2024-04-16 19:43:01 +0800

    Add Directive Description File

--- src/instructions.c
+++ src/instructions.c
@@ -7,146 +7,58 @@
 
 R_INSTRUCTION as_r_instruction(uint32_t instruction) {
     return (R_INSTRUCTION) {
-        .opcode = (
-            // <TotalBits=6>
-            // <Start=0> <End=6> <Length=6> <Offset=0> <Zero=false>
-            ((instruction << 0) & (0xFFFFFFFF >> (32 + 0 - 6)) << 0)
+        .op0 = (
+            // <TotalBits=3>
+            // <Start=0> <End=3> <Length=3> <Offset=0> <Zero=false>
+            ((instruction << 0) & (0xFFFFFFFF >> (32 + 0 - 3)) << 0)
             |
             // Unsigned
             0
         ),
         .rd = (
-            // <TotalBits=4>
-            // <Start=7> <End=11> <Length=4> <Offset=0> <Zero=false>
-            ((instruction << 7) & (0xFFFFFFFF >> (32 + 7 - 11)) << 0)
-            |
-            // Unsigned
-            0
-        ),
-        .func3 = (
-            // <TotalBits=2>
-            // <Start=12> <End=14> <Length=2> <Offset=0> <Zero=false>
-            ((instruction << 12) & (0xFFFFFFFF >> (32 + 12 - 14)) << 0)
+            // <TotalBits=6>
+            // <Start=4> <End=10> <Length=6> <Offset=0> <Zero=false>
+            ((instruction << 4) & (0xFFFFFFFF >> (32 + 4 - 10)) << 0)
             |
             // Unsigned
             0
         ),
         .rs1 = (
-            // <TotalBits=4>
-            // <Start=15> <End=19> <Length=4> <Offset=0> <Zero=false>
-            ((instruction << 15) & (0xFFFFFFFF >> (32 + 15 - 19)) << 0)
+            // <TotalBits=6>
+            // <Start=11> <End=17> <Length=6> <Offset=0> <Zero=false>
+            ((instruction << 11) & (0xFFFFFFFF >> (32 + 11 - 17)) << 0)
             |
             // Unsigned
             0
         ),
         .rs2 = (
-            // <TotalBits=4>
-            // <Start=20> <End=24> <Length=4> <Offset=0> <Zero=false>
-            ((instruction << 20) & (0xFFFFFFFF >> (32 + 20 - 24)) << 0)
-            |
-            // Unsigned
-            0
-        ),
-        .func7 = (
             // <TotalBits=6>
-            // <Start=25> <End=31> <Length=6> <Offset=0> <Zero=false>
-            ((instruction << 25) & (0xFFFFFFFF >> (32 + 25 - 31)) << 0)
-            |
-            // Unsigned
-            0
-        )
-    };
-}
-
-I_INSTRUCTION as_i_instruction(uint32_t instruction) {
-    return (I_INSTRUCTION) {
-        .opcode = (
-            // <TotalBits=7>
-            // <Start=0> <End=7> <Length=7> <Offset=0> <Zero=false>
-            ((instruction << 0) & (0xFFFFFFFF >> (32 + 0 - 7)) << 0)
+            // <Start=18> <End=24> <Length=6> <Offset=0> <Zero=false>
+            ((instruction << 18) & (0xFFFFFFFF >> (32 + 18 - 24)) << 0)
             |
             // Unsigned
             0
         ),
-        .rd = (
-            // <TotalBits=5>
-            // <Start=6> <End=11> <Length=5> <Offset=0> <Zero=false>
-            ((instruction << 6) & (0xFFFFFFFF >> (32 + 6 - 11)) << 0)
-            |
-            // Unsigned
-            0
-        ),
-        .func3 = (
+        .op1 = (
             // <TotalBits=2>
-            // <Start=12> <End=14> <Length=2> <Offset=0> <Zero=false>
-            ((instruction << 12) & (0xFFFFFFFF >> (32 + 12 - 14)) << 0)
+            // <Start=25> <End=27> <Length=3> <Offset=0> <Zero=false>
+            ((instruction << 25) & (0xFFFFFFFF >> (32 + 25 - 27)) << 0)
             |
             // Unsigned
             0
         ),
-        .rs1 = (
-            // <TotalBits=4>
-            // <Start=15> <End=19> <Length=4> <Offset=0> <Zero=false>
-            ((instruction << 15) & (0xFFFFFFFF >> (32 + 15 - 19)) << 0)
+        .op2 = (
+            // <TotalBits=1>
+            // <Start=28> <End=29> <Length=1> <Offset=0> <Zero=false>
+            ((instruction << 28) & (0xFFFFFFFF >> (32 + 28 - 29)) << 0)
             |
             // Unsigned
             0
         ),
-        .imm = (
-            // <TotalBits=7>
-            // <Start=null> <End=null> <Length=1> <Offset=0> <Zero=true>
-            0
-            |
-            // <Start=25> <End=31> <Length=6> <Offset=1> <Zero=false>
-            ((instruction << 25) & (0xFFFFFFFF >> (32 + 25 - 31)) << 1)
-            |
-            // Signed <Bits=7>
-            ((instruction >> (7 - 1)) == 0 ? 0 : (0xFFFFFFFF << 7))
-        )
-    };
-}
-
-S_INSTRUCTION as_s_instruction(uint32_t instruction) {
-    return (S_INSTRUCTION) {
-        .opcode = (
-            // <TotalBits=6>
-            // <Start=0> <End=6> <Length=6> <Offset=0> <Zero=false>
-            ((instruction << 0) & (0xFFFFFFFF >> (32 + 0 - 6)) << 0)
-            |
-            // Unsigned
-            0
-        ),
-        .func3 = (
-            // <TotalBits=2>
-            // <Start=12> <End=14> <Length=2> <Offset=0> <Zero=false>
-            ((instruction << 12) & (0xFFFFFFFF >> (32 + 12 - 14)) << 0)
-            |
-            // Unsigned
-            0
-        ),
-        .rs1 = (
-            // <TotalBits=4>
-            // <Start=15> <End=19> <Length=4> <Offset=0> <Zero=false>
-            ((instruction << 15) & (0xFFFFFFFF >> (32 + 15 - 19)) << 0)
-            |
-            // Unsigned
-            0
-        ),
-        .rs2 = (
-            // <TotalBits=4>
-            // <Start=20> <End=24> <Length=4> <Offset=0> <Zero=false>
-            ((instruction << 20) & (0xFFFFFFFF >> (32 + 20 - 24)) << 0)
-            |
-            // Unsigned
-            0
-        ),
-        .imm = (
-            // <TotalBits=10>
-            // <Start=7> <End=11> <Length=4> <Offset=0> <Zero=false>
-            ((instruction << 7) & (0xFFFFFFFF >> (32 + 7 - 11)) << 0)
-            |
-            // <Start=25> <End=31> <Length=6> <Offset=4> <Zero=false>
-            ((instruction << 25) & (0xFFFFFFFF >> (32 + 25 - 31)) << 4)
+        .rnd = (
+            // <TotalBits=1>
+            // <Start=30> <End=31> <Length=1> <Offset=0> <Zero=false>
+            ((instruction << 30) & (0xFFFFFFFF >> (32 + 30 - 31)) << 0)
             |
             // Unsigned
             0
@@ -154,22 +66,119 @@ S_INSTRUCTION as_s_instruction(uint32_t instruction) {
     };
 }
 
-bool is_add_instruction(uint32_t instruction, R_INSTRUCTION* decoded_instruction) {
-    if (decoded_instruction->opcode != 0b0110011) return false;
-    if (decoded_instruction->func3 != 0b000) return false;
-    if (decoded_instruction->func7 != 0b0000000) return false;
+// I_INSTRUCTION as_i_instruction(uint32_t instruction) {
+//     return (I_INSTRUCTION) {
+//         .opcode = (
+//             // <TotalBits=7>
+//             // <Start=0> <End=7> <Length=7> <Offset=0> <Zero=false>
+//             ((instruction << 0) & (0xFFFFFFFF >> (32 + 0 - 7)) << 0)
+//             |
+//             // Unsigned
+//             0
+//         ),
+//         .rd = (
+//             // <TotalBits=5>
+//             // <Start=6> <End=11> <Length=5> <Offset=0> <Zero=false>
+//             ((instruction << 6) & (0xFFFFFFFF >> (32 + 6 - 11)) << 0)
+//             |
+//             // Unsigned
+//             0
+//         ),
+//         .func3 = (
+//             // <TotalBits=2>
+//             // <Start=12> <End=14> <Length=2> <Offset=0> <Zero=false>
+//             ((instruction << 12) & (0xFFFFFFFF >> (32 + 12 - 14)) << 0)
+//             |
+//             // Unsigned
+//             0
+//         ),
+//         .rs1 = (
+//             // <TotalBits=4>
+//             // <Start=15> <End=19> <Length=4> <Offset=0> <Zero=false>
+//             ((instruction << 15) & (0xFFFFFFFF >> (32 + 15 - 19)) << 0)
+//             |
+//             // Unsigned
+//             0
+//         ),
+//         .imm = (
+//             // <TotalBits=7>
+//             // <Start=null> <End=null> <Length=1> <Offset=0> <Zero=true>
+//             0
+//             |
+//             // <Start=25> <End=31> <Length=6> <Offset=1> <Zero=false>
+//             ((instruction << 25) & (0xFFFFFFFF >> (32 + 25 - 31)) << 1)
+//             |
+//             // Signed <Bits=7>
+//             ((instruction >> (7 - 1)) == 0 ? 0 : (0xFFFFFFFF << 7))
+//         )
+//     };
+// }
+
+// S_INSTRUCTION as_s_instruction(uint32_t instruction) {
+//     return (S_INSTRUCTION) {
+//         .opcode = (
+//             // <TotalBits=6>
+//             // <Start=0> <End=6> <Length=6> <Offset=0> <Zero=false>
+//             ((instruction << 0) & (0xFFFFFFFF >> (32 + 0 - 6)) << 0)
+//             |
+//             // Unsigned
+//             0
+//         ),
+//         .func3 = (
+//             // <TotalBits=2>
+//             // <Start=12> <End=14> <Length=2> <Offset=0> <Zero=false>
+//             ((instruction << 12) & (0xFFFFFFFF >> (32 + 12 - 14)) << 0)
+//             |
+//             // Unsigned
+//             0
+//         ),
+//         .rs1 = (
+//             // <TotalBits=4>
+//             // <Start=15> <End=19> <Length=4> <Offset=0> <Zero=false>
+//             ((instruction << 15) & (0xFFFFFFFF >> (32 + 15 - 19)) << 0)
+//             |
+//             // Unsigned
+//             0
+//         ),
+//         .rs2 = (
+//             // <TotalBits=4>
+//             // <Start=20> <End=24> <Length=4> <Offset=0> <Zero=false>
+//             ((instruction << 20) & (0xFFFFFFFF >> (32 + 20 - 24)) << 0)
+//             |
+//             // Unsigned
+//             0
+//         ),
+//         .imm = (
+//             // <TotalBits=10>
+//             // <Start=7> <End=11> <Length=4> <Offset=0> <Zero=false>
+//             ((instruction << 7) & (0xFFFFFFFF >> (32 + 7 - 11)) << 0)
+//             |
+//             // <Start=25> <End=31> <Length=6> <Offset=4> <Zero=false>
+//             ((instruction << 25) & (0xFFFFFFFF >> (32 + 25 - 31)) << 4)
+//             |
+//             // Unsigned
+//             0
+//         )
+//     };
+// }
+
+bool is_fadd_instruction(uint32_t instruction, R_INSTRUCTION* decoded_instruction) {
+    if (decoded_instruction->op0 != 0b0000) return false;
+    if (decoded_instruction->op1 != 0b0111) return false;
+    if (decoded_instruction->op2 != 0b00) return false;
+    if (decoded_instruction->rnd != 0b00) return false;
     return true;
 }
 
-bool is_sub_instruction(uint32_t instruction, R_INSTRUCTION* decoded_instruction) {
-    if (decoded_instruction->opcode != 0b0110011) return false;
-    if (decoded_instruction->func3 != 0b000) return false;
-    if (decoded_instruction->func7 != 0b0100000) return false;
-    return true;
-}
+// bool is_sub_instruction(uint32_t instruction, R_INSTRUCTION* decoded_instruction) {
+//     if (decoded_instruction->opcode != 0b0110011) return false;
+//     if (decoded_instruction->func3 != 0b000) return false;
+//     if (decoded_instruction->func7 != 0b0100000) return false;
+//     return true;
+// }
 
-bool is_lw_instruction(uint32_t instruction, I_INSTRUCTION* decoded_instruction) {
-    if (decoded_instruction->opcode != 0b0000011) return false;
-    if (decoded_instruction->func3 != 0b010) return false;
-    return true;
-}
+// bool is_lw_instruction(uint32_t instruction, I_INSTRUCTION* decoded_instruction) {
+//     if (decoded_instruction->opcode != 0b0000011) return false;
+//     if (decoded_instruction->func3 != 0b010) return false;
+//     return true;
+// }
--- src/instructions.h
+++ src/instructions.h
@@ -10,61 +10,64 @@
 #include <stdint.h>
 
 typedef struct {
-    // <TotalBits=6>
-    uint8_t opcode;
     // <TotalBits=4>
+    uint8_t op0;
+    // <TotalBits=7>
     uint8_t rd;
-    // <TotalBits=2>
-    uint8_t func3;
-    // <TotalBits=4>
+    // <TotalBits=7>
     uint8_t rs1;
-    // <TotalBits=4>
+    // <TotalBits=7>
     uint8_t rs2;
-    // <TotalBits=6>
-    uint8_t func7;
+    // <TotalBits=3>
+    uint8_t op1;
+    // <TotalBits=2>
+    uint8_t op2;
+    // <TotalBits=2>
+    uint8_t rnd;
 } R_INSTRUCTION;
 R_INSTRUCTION as_r_instruction(uint32_t instruction);
 
-typedef struct {
-    // <TotalBits=7>
-    uint8_t opcode;
-    // <TotalBits=5>
-    uint8_t rd;
-    // <TotalBits=2>
-    uint8_t func3;
-    // <TotalBits=4>
-    uint8_t rs1;
-    // <TotalBits=7>
-    int8_t imm;
-} I_INSTRUCTION;
-I_INSTRUCTION as_i_instruction(uint32_t instruction);
+// typedef struct {
+//     // <TotalBits=7>
+//     uint8_t opcode;
+//     // <TotalBits=5>
+//     uint8_t rd;
+//     // <TotalBits=2>
+//     uint8_t func3;
+//     // <TotalBits=4>
+//     uint8_t rs1;
+//     // <TotalBits=7>
+//     int8_t imm;
+// } I_INSTRUCTION;
+// I_INSTRUCTION as_i_instruction(uint32_t instruction);
 
-typedef struct {
-    // <TotalBits=6>
-    uint8_t opcode;
-    // <TotalBits=2>
-    uint8_t func3;
-    // <TotalBits=4>
-    uint8_t rs1;
-    // <TotalBits=4>
-    uint8_t rs2;
-    // <TotalBits=10>
-    uint16_t imm;
-} S_INSTRUCTION;
-S_INSTRUCTION as_s_instruction(uint32_t instruction);
+// typedef struct {
+//     // <TotalBits=6>
+//     uint8_t opcode;
+//     // <TotalBits=2>
+//     uint8_t func3;
+//     // <TotalBits=4>
+//     uint8_t rs1;
+//     // <TotalBits=4>
+//     uint8_t rs2;
+//     // <TotalBits=10>
+//     uint16_t imm;
+// } S_INSTRUCTION;
+// S_INSTRUCTION as_s_instruction(uint32_t instruction);
 
-const uint8_t OPCODE_ADD = 0b0110011;
-const uint8_t FUNC3_ADD = 0b000;
-const uint8_t FUNC7_ADD = 0b0000000;
+const uint8_t OP0_FADD = 0b0000;
+const uint8_t OP1_FADD = 0b111;
+const uint8_t OP2_FADD = 0b00;
+const uint8_t RND_FADD = 0b00;
 bool is_add_instruction(uint32_t instruction, R_INSTRUCTION* decoded_instruction);
 
-const uint8_t OPCODE_SUB = 0b0110011;
-const uint8_t FUNC3_SUB = 0b000;
-const uint8_t FUNC7_SUB = 0b0100000;
-bool is_sub_instruction(uint32_t instruction, R_INSTRUCTION* decoded_instruction);
+// const uint8_t OPCODE_SUB = 0b0110011;
+// const uint8_t FUNC3_SUB = 0b000;
+// const uint8_t FUNC7_SUB = 0b0100000;
+// bool is_sub_instruction(uint32_t instruction, R_INSTRUCTION* decoded_instruction);
 
-const uint8_t OPCODE_LW = 0b0000011;
-const uint8_t FUNC3_LW = 0b010;
-bool is_lw_instruction(uint32_t instruction, I_INSTRUCTION* decoded_instruction);
+// const uint8_t OPCODE_LW = 0b0000011;
+// const uint8_t FUNC3_LW = 0b010;
+// bool is_lw_instruction(uint32_t instruction, I_INSTRUCTION* decoded_instruction);
 
 #endif
