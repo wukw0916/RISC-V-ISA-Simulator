57b7b9a8ae0f3ac8d9f0f35b0d69ad2a1774a524
wukw <wukw@ljmicro.cn>
2024-04-17 14:04:46 +0800

    fix debug

--- src/simulator.c
+++ src/simulator.c
@@ -48,6 +48,9 @@ void display_memory(simulator* s, uint32_t start_addr, uint32_t length) {
     printf("%s\n", line);
 }
 
+// Disable pointer math warnings only for mem access functions
+#pragma GCC diagnostic ignored "-Wpointer-arith"
+
 // Generic write memory function
 // Use write_word, write_hword, write_byte instead
 #define write_memory(s, addr, data, data_type) {\
@@ -85,10 +88,13 @@ int32_t read_sword(simulator* s, uint32_t addr) read_memory(s, addr, int32_t)
 int16_t read_shword(simulator* s, uint32_t addr) read_memory(s, addr, int16_t)
 int8_t read_sbyte(simulator* s, uint32_t addr) read_memory(s, addr, int8_t)
 
+// Enable warnings for any other pointer math
+#pragma GCC diagnostic warning "-Wpointer-arith"
+
 // register read
 uint32_t read_register(simulator* s, REGISTER reg) {
     if (reg > 32)
-        FAIL("Invalid register read");
+        FAIL("Invalid register read: %d", reg);
     if (reg == REG_ZERO)
         return 0;
     return s->reg[reg];
@@ -97,7 +103,7 @@ uint32_t read_register(simulator* s, REGISTER reg) {
 // register write
 void write_register(simulator* s, REGISTER reg, uint32_t data) {
     if (reg > 32)
-        FAIL("Invalid register write");
+        FAIL("Invalid register write: %d", reg);
     if (reg == REG_ZERO)
         return;
     s->reg[reg] = data;
