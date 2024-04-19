4e0db15a6071ca8678f1365c570ddf76451887e3
wukw <wukw@ljmicro.cn>
2024-04-19 13:58:09 +0800

    Add the automatic.sh script to support engineering automation

--- automatic.sh
+++ automatic.sh
@@ -4,6 +4,16 @@
 echo "run testcase"
 ./build/egp01_simulator --verbose --load-file example.mem
 
+# 下载运行opcode_generator工具的相关依赖库
+echo "install yq"
+pip3 install --user yq
+echo "set envirment path"
+curl -L 'https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64' > $HOME/.local/bin/jq
+echo "echo "Grant permissions to scripts""
+chmod +x $HOME/.local/bin/jq
+# Regenerate the opcodes
+PATH="$HOME/.local/bin:$PATH" bash ./tools/opcode_generator
+
 # 编译依赖库
 echo "compiler dependency package"
 gcc -c src/instructions.c
